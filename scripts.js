5ed37f15c77492ffa5c7370a6a6dc180cb0cf08b
Gammanik <gammanik@gmail.com>
2024-04-05 17:37:42 +0300

    buttons below

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-function toggleInfo(elementId) {
-    var info = document.getElementById(elementId);
-    var content = info.querySelector('.scrollable-content');
-    if (info.classList.contains('show')) {
-        info.classList.remove('show');
-        setTimeout(() => content.style.opacity = 0, 50);
-    } else {
-        info.classList.add('show');
-        setTimeout(() => content.style.opacity = 1, 250);
-    }
+function switchTab(tabName) {
+    document.querySelectorAll('.tab-content').forEach(function(node) {
+        node.style.display = 'none';
+    });
+    document.getElementById(tabName + '-content').style.display = 'block';
+
+    document.querySelectorAll('.nav-item').forEach(function(node) {
+        node.classList.remove('active');
+    });
+    document.querySelector('.nav-item[onclick="switchTab(\'' + tabName + '\')"]').classList.add('active');
 }
+
+// Активируем первую вкладку по умолчанию
+switchTab('home');
