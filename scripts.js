f75f87f10ff94538c907f28c8b940619a34a484a
Gammanik <gammanik@gmail.com>
2024-04-05 17:22:22 +0300

    property name sliding card

--- scripts.js
+++ scripts.js
@@ -1,33 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const headings = document.querySelectorAll('.menu-heading');
-    headings.forEach(function(heading) {
-        heading.addEventListener('click', function() {
-            const submenu = this.nextElementSibling;
-            const blurElements = document.querySelectorAll('.container > *:not(.menu)');
-            if (submenu.classList.contains('active')) {
-                submenu.classList.remove('active');
-                blurElements.forEach(element => {
-                    element.style.filter = 'none';
-                });
-            } else {
-                submenu.classList.add('active');
-                blurElements.forEach(element => {
-                    element.style.filter = 'blur(5px)';
-                });
-            }
-        });
-    });
-
-    const buttons = document.querySelectorAll('.menu-heading');
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            const submenu = this.nextElementSibling;
-            const cardContent = submenu.querySelector('.card-content');
-            const cardTitle = cardContent.querySelector('.info');
-            const cardDescription = cardContent.querySelector('.card-description');
-
-            // cardTitle.textContent = this.getAttribute('data-title');
-            // cardDescription.textContent = cardContent.getAttribute('data-description');
-        });
-    });
-});
+function toggleInfo(elementId) {
+    var info = document.getElementById(elementId);
+    var content = info.querySelector('.scrollable-content');
+    if (info.classList.contains('show')) {
+        info.classList.remove('show');
+        setTimeout(() => content.style.opacity = 0, 50);
+    } else {
+        info.classList.add('show');
+        setTimeout(() => content.style.opacity = 1, 250);
+    }
+}
