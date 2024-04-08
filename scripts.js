c07950049cad50f478045b6cab397dedcc0ca1fc
Gammanik <gammanik@gmail.com>
2024-04-08 23:08:37 +0300

    home/home as a separate page

--- scripts.js
+++ scripts.js
@@ -10,7 +10,28 @@ function switchTab(tabName) {
     document.querySelector(`.nav-item[onclick="switchTab('${tabName}')"]`).classList.add('active');
 }
 
-// Активация первой вкладки при загрузке страницы
+async function loadTabContent(tabName) {
+    const response = await fetch(`${tabName}/${tabName}.html`);
+    if (response.ok) {
+        const content = await response.text();
+        document.getElementById(`${tabName}-content`).innerHTML = content;
+
+        // Проверяем, загружаем ли мы вкладку 'home'
+        if (tabName === 'home') {
+            // Создаем элемент <link> для подключения CSS
+            const cssLink = document.createElement('link');
+            cssLink.rel = 'stylesheet';
+            cssLink.href = `${tabName}/home-styles.css`; // Путь к CSS файлу
+            document.head.appendChild(cssLink); // Добавляем <link> в <head>
+        }
+    } else {
+        console.error('Ошибка загрузки:', response.statusText);
+    }
+}
+
+
+// // Активация первой вкладки при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
     switchTab('home');
+    loadTabContent('home'); // Загрузить содержимое для домашней страницы
 });
